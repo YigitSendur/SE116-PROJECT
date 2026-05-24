05255959f6914f55e73206ca4663b279e05e50a2
gokberken <gokberken94@gmail.com>
2026-05-24 15:53:14 +0300

    ServiceProvider Class

---
+++ src/services/ServiceProvider.java
@@ -0,0 +1,21 @@
+package services;
+
+import cells.Cell;
+
+public abstract class ServiceProvider extends Cell {
+    protected int radius;
+
+    public ServiceProvider(int x,int y,int radius){
+        super(x,y);
+        this.radius = radius;
+    }
+
+    @Override
+    public boolean isConnectable() {
+        return false;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
