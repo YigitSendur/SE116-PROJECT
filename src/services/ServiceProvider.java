423882f7fe3c9c03503f31c65c45e24f0680544d
Eren Karlı <erenkarli123456789@gmail.com>
2026-05-30 02:46:32 +0300

    ServiceProvider'a provideService ve euclidean mesafe metodu eklendi

--- src/services/ServiceProvider.java
+++ src/services/ServiceProvider.java
@@ -1,21 +1,47 @@
 package services;
 
 import cells.Cell;
+import city.Grid;
+import zones.Zone;
 
 public abstract class ServiceProvider extends Cell {
     protected int radius;
 
-    public ServiceProvider(int x,int y,int radius){
-        super(x,y);
+    public ServiceProvider(int x, int y, int radius){
+        super(x, y);
         this.radius = radius;
     }
 
     @Override
-    public boolean isConnectable() {
+    public boolean isConnectable(){
         return false;
     }
 
-    public int getRadius() {
+    public int getRadius(){
         return radius;
     }
+
+    public abstract String getServiceType();
+
+    public void provideService(Grid grid){
+        for(int row = 0; row < grid.getHeight(); row++){
+            for(int col = 0; col < grid.getWidth(); col++){
+                Cell cell = grid.getCell(col, row);
+                if(cell == null) continue;
+
+                double dist = euclideanDistance(col, row);
+                if(dist <= this.radius){
+                    applyServiceToCell(cell);
+                }
+            }
+        }
+    }
+
+    private double euclideanDistance(int targetX, int targetY){
+        int dx = targetX - this.coordinateX;
+        int dy = targetY - this.coordinateY;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    protected abstract void applyServiceToCell(Cell cell);
 }
