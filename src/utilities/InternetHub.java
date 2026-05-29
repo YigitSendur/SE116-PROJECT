e8eca0c1a728291b3cd92dc1ca6df1661b6fe1ef
Eren Karlı <erenkarli123456789@gmail.com>
2026-05-30 01:49:39 +0300

    InternetHub sinifi olusturuldu

---
+++ src/utilities/InternetHub.java
@@ -0,0 +1,13 @@
+package utilities;
+
+public class InternetHub extends UtilityProvider {
+
+    public InternetHub(int x, int y){
+        super(x, y);
+    }
+
+    @Override
+    public String getSymbol(){
+        return "T";
+    }
+}
