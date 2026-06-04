7d85c14d7005f8efaaa1430b5ef67e9a08060225
huseyinesen-code <huseyin.esen@std.izmirekonomi.edu.tr>
2026-06-04 13:02:06 +0300

    Cell

---
+++ src/main/Main.java
@@ -0,0 +1,37 @@
+package main;
+import city.Grid;
+import city.InvalidMapException;
+import engine.SimulationEngine;
+
+public class Main {
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.err.println("Tick numbers are invalid");
+        }
+
+        String mapFile = args[0];
+        int ticks;
+
+        try {
+            ticks = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.err.println("Tick number must be integer: " + args[1]);
+            return;
+        }
+
+        if (ticks <= 0) {
+            System.err.println("Tick number must be positive " + ticks);
+
+            return;
+        }
+
+        try {
+            Grid grid = MapLoader.loadMap(mapFile);
+            SimulationEngine engine = new SimulationEngine(grid, ticks);
+            engine.run();
+        } catch (InvalidMapException e) {
+            System.err.println("Map error: " + e.getMessage());
+        }
+    }
+}
